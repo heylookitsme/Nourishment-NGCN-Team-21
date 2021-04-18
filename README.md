65d11332cc518b935bf3c13ca93699df84a5e288
heylookitsme <heylookitsme@github.com>
2021-04-18 18:21:37 -0400

    refactored everything
    more elaborate commit message to come later

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NGCN-Team-21
-Project for Next Gen Coders network. Addresses global food and water security. 
+Project for Next Gen Coders network. Addresses global food and water security.
+
--- food_security/urls.py
+++ food_security/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='homepage'),
     path('admin/', admin.site.urls),
+#    path('support/', include('campaigns.urls')),
     path('users/', include('users.urls')),
+    path('learn/', views.learn),
+    path('resources/', views.resources),
+    path('about/', views.about),
 
 ]
