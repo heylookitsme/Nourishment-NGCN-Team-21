1b9c3b245ce8830337b149306e2ab6091d1dba53
Jenin A. Bostan <80278799+jenin-bostan@users.noreply.github.com>
2021-04-20 18:22:35 +0300

    Add files via upload
    page 3

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 new WOW().init();
 
+
+$(document).ready(function () {
+    new WOW().init();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+});
+
 // Show Or Hide Element Based on ScrollTop
 $(document).scroll(function () {
      if ($(document).scrollTop() > 300) {
