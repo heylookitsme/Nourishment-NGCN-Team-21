afd2ccb1ea76a0ddabf65744dce658deb92ffd15
hunar <35507228+namopy@users.noreply.github.com>
2021-04-23 15:33:16 +0300

    Add files via upload

--- food_security/settings.py
+++ food_security/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'campaigns.apps.CampaignsConfig',
+    'widget_tweaks',
 
 
 ]
@@ -122,9 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+
+] #from where to get all static content
+
+STATIC_ROOT = os.path.join(BASE_DIR,'assets') #where to put static files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 AUTH_USER_MODEL = 'users.User'
+
+
